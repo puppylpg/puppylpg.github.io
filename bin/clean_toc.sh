af8a6aaad7245dd076e0b1e84a08a50c91b30940
puppylpg <shininglhb@163.com>
2024-05-13 11:55:41 +0800

    clean_toc.sh for multiple directories

--- bin/clean_toc.sh
+++ bin/clean_toc.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-POST_DIR=$(dirname "$(dirname "$(realpath "$0")")")/_posts
-echo $POST_DIR
+DIRECTORIES=("_posts" "_tutorials")
 
-# 检测一个文件夹下所有的md格式的文件
-for file in $POST_DIR/*.md; do
-    # 检查文件第一行是否包含 [toc]
-    if [[ $(head -n 1 "$file") == "[toc]" ]]; then
-        # 删除第一行以及后面的空行
-        sed -i -e '1,/^$/d' "$file"
-    fi
+for dir in "${DIRECTORIES[@]}"; do
+    DIR=$(dirname "$(dirname "$(realpath "$0")")")/$dir
+    echo $DIR
+
+    for file in $DIR/*.md; do
+        if [[ $(head -n 1 "$file") == "[toc]" ]]; then
+            sed -i -e '1,/^$/d' "$file"
+        fi
+    done
 done
+
